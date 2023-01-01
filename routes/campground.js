73816f445b05ec05b29012ac3362278f3f9eca6e
Abhisin90 <abhinavsingh0901@gmail.com>
2023-01-02 03:29:28 +0530

    Added Controllers

--- routes/campground.js
+++ routes/campground.js
@@ -1,67 +1,22 @@
 const express = require('express')
 const router = express.Router()
-const Campground = require('../models/campground')
 const catchAsync = require('../utils/catchAsync')
 const {isLoggedIn,isOwner,validateCampground} = require('../middleware')
+const campgrounds = require('../controllers/campgrounds')
 
 // catchAsync --> function for catching the error , to reduce the repetitive use of try and catch
-router.get('/',catchAsync(async (req,res) => {
-    const campgrounds = await Campground.find({})
-    res.render('campgrounds/index',{campgrounds})
-}))
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn,validateCampground,catchAsync(campgrounds.createCampground))
 
-router.get('/new',isLoggedIn,(req,res) => {
-    res.render('campgrounds/new')
-})
+router.get('/new',isLoggedIn,campgrounds.renderNewForm)
 
-// populate helps to showcase documents as whole rather than just their objectIds
-router.get('/:id',catchAsync(async (req,res) => {
-    const {id} = req.params
-    const foundCamp = await Campground.findById(id)
-    .populate({
-        path:'reviews',
-        populate:{
-            path:'owner'
-        }
-    }).populate('owner')
-    if(!foundCamp) {
-        req.flash('error','No such campground')
-        return res.redirect('/campgrounds')
-    }
-    res.render('campgrounds/show',{foundCamp})
-}))
+// using middleware to first verify if it is logged in and then check for if it is owner and then verify input data from form
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn,isOwner,validateCampground,catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn,isOwner,catchAsync(campgrounds.deleteCampground))
 
-router.get('/:id/edit',isLoggedIn,isOwner,catchAsync(async (req,res) => {
-    const {id} = req.params
-    const foundCamp = await Campground.findById(id)
-    if(!foundCamp) {
-        req.flash('error','No such campground')
-        return res.redirect('/campgrounds')
-    }
-    res.render('campgrounds/edit',{foundCamp})
-}))
+router.get('/:id/edit',isLoggedIn,isOwner,catchAsync(campgrounds.renderEditForm))
 
-// using middleware to first verify input data from form
-router.put('/:id',isLoggedIn,isOwner,validateCampground,catchAsync(async (req,res) => {
-    const {id} = req.params
-    const updatedCamp = await Campground.findByIdAndUpdate(id,req.body.campground)
-    req.flash('success','Successfully updated the campground')
-    res.redirect(`/campgrounds/${updatedCamp.id}`) 
-}))
-
-router.post('/',isLoggedIn,validateCampground,catchAsync(async (req,res) => {
-    const campground =  new Campground(req.body.campground)
-    campground.owner = req.user._id
-    await campground.save()
-    req.flash('success','Successfully made a new campground')
-    res.redirect(`/campgrounds/${campground.id}`)
-}))
-
-router.delete('/:id',isLoggedIn,isOwner,catchAsync(async (req,res) => {
-    const {id} = req.params
-    await Campground.findByIdAndDelete(id)
-    req.flash('success','Successfully deleted campground')
-    res.redirect("/campgrounds")
-}))
-
-module.exports = router
+module.exports = router 
--- routes/review.js
+++ routes/review.js
@@ -1,28 +1,11 @@
 const express = require('express')
 const router = express.Router({mergeParams:true})
-const Review = require('../models/review')
-const Campground = require('../models/campground')
 const catchAsync = require('../utils/catchAsync')
 const {isLoggedIn,isReviewOwner,validateReview} = require('../middleware')
+const reviews = require('../controllers/reviews')
 
-router.post('/',isLoggedIn,validateReview,catchAsync(async (req,res) => {    
-    const foundCamp = await Campground.findById(req.params.id)
-    const review = new Review(req.body.review)
-    review.owner = req.user.id
-    foundCamp.reviews.push(review)
-    await review.save()
-    await foundCamp.save()
-    req.flash('success','Review created')
-    res.redirect(`/campgrounds/${foundCamp.id}`)
-}))
+router.post('/',isLoggedIn,validateReview,catchAsync(reviews.createReview))
 
-// pull helps to delete particular reviews with matching pattern as given
-router.delete('/:reviewId',isLoggedIn,isReviewOwner,async(req,res) => {
-    const {id,reviewId} = req.params
-    await Campground.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
-    await Review.findByIdAndDelete(reviewId)
-    req.flash('success','Successfully deleted Review')
-    res.redirect(`/campgrounds/${id}`)
-})
+router.delete('/:reviewId',isLoggedIn,isReviewOwner,catchAsync(reviews.deleteReview))
 
 module.exports = router
